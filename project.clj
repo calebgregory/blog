f75358f97f8b2becf31085d9904f759a79dca097
calebgregory <calebgregory@gmail.com>
2016-04-17 19:23:50 -0500

    Adding profile.clj changes for  lib

--- project.clj
+++ project.clj
@@ -4,10 +4,13 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.0"]
+                 [environ "1.0.2"]
+                 [com.novemberain/monger "3.0.2"]
                  [ring/ring-defaults "0.1.5"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler blog.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
-)
+                        [ring/ring-mock "0.3.0"]]}
+   :default [:base :system :user :provided :dev :local-dev]}
+  :aliases {"test" ["with-profile" "+local-test" "test"]})
